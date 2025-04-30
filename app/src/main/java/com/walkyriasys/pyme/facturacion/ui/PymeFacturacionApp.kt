a4a9ce06bc3246f06fe244c7dfcccca3508d3e85
Danilo Dominguez <danilo.dope@gmail.com>
2025-04-30 10:22:18 -0500

    Use a SnackbarHost instead of a Scaffold

--- app/src/main/java/com/walkyriasys/pyme/facturacion/ui/PymeFacturacionApp.kt
+++ app/src/main/java/com/walkyriasys/pyme/facturacion/ui/PymeFacturacionApp.kt
@@ -4,13 +4,14 @@ import PymeFacturacionNavHost
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.walkyriasys.pyme.facturacion.navigation.NavControllerNavigator
 import com.walkyriasys.pyme.facturacion.navigation.Navigator
@@ -25,20 +26,20 @@ fun PymeFacturacionApp(snackbarHostState: SnackbarHostState) {
         val navigator = NavControllerNavigator(navController)
 
         CompositionLocalProvider(LocalNavigator provides navigator) {
-            Scaffold(
-                snackbarHost = {
-                    SnackbarHost(hostState = snackbarHostState)
-                },
-            ) { contentPadding ->
-                Box(
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                PymeFacturacionNavHost(
+                    navController = navController,
+                )
+
+                SnackbarHost(
+                    hostState = snackbarHostState,
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(contentPadding)
-                ) {
-                    PymeFacturacionNavHost(
-                        navController = navController,
-                    )
-                }
+                        .align(Alignment.BottomCenter)
+                        .padding(bottom = 5.dp)
+                )
             }
         }
     }
